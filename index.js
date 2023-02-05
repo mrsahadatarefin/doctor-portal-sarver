0f82fc42ad71433976158f887750cd10bfc20a41
mrsahadatarefin <sahadatarifin806@gmail.com>
2023-02-05 23:41:00 +0600

    jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require ('jsonwebtoken')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
@@ -21,11 +22,59 @@ async function run (){
 try{
 
 const appointmentOptionCollection = client.db('doctorPortal').collection('appointmentOptions');
-
+const bookingsCollection = client.db('doctorPortal').collection('bookings');
+const usersCollection = client.db('doctorPortal').collection('users');
 app.get('/appointmentOptions',async(req,res)=>{
-   const query ={} 
+   const data = req.query.data;
+   console.log(data)
+    const query ={} 
    const  options = await appointmentOptionCollection.find(query).toArray();
    res.send(options)
+
+});
+
+app.get('/bookings',async(req,res)=>{
+    console.log(req)
+    const email = req.query.email;
+    const query = {email:email};
+    const bookings = await bookingsCollection.find(query).toArray();
+    res.send(bookings)
+
+    console.log(req)
+})
+
+
+app.post('/bookings', async (req,res)=>{
+const booking =req.body;
+const query ={
+    appointmentDate:booking.AppointmentDate
+} 
+const AlreadyBooked = await bookingsCollection.find(query).toArray()
+if(AlreadyBooked.length){
+    const message = `you already have a booking on ${booking.appointmentDate}`
+    res.send({acknowledged:false,message})
+}
+const result = await bookingsCollection.insertOne(booking);
+res.send(result);
+})
+
+app.get('/jwt',async(req,res)=>{
+    const email = req.query.email;
+    const query= {email:email};
+    const user = await usersCollection.findOne(query)
+   if(user){
+    const token = jwt.sign({email}, process.env.ACCESS_TOKEN,{expiresIn:'1h'})
+    return res.send({ACCESS_TOKEN:token});
+   }
+   
+    res.status(403).send({token:""})
+
+})
+
+app.post('/users',async(req,res)=>{
+   const user = req.body;
+   const result = await usersCollection.insertOne(user)
+   res.send(result)
 
 })
 
